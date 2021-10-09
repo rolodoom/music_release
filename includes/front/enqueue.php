7628ce0aaa6ca9cb3620bfe44581352a4bcc1a56
rolodoom <rolodoom@gmail.com>
2021-10-09 11:17:53 -0500

    Update

--- includes/front/enqueue.php
+++ includes/front/enqueue.php
@@ -6,6 +6,7 @@ add_action( 'wp_enqueue_scripts', 'musicrelease_enqueue_scripts', 100 );
 
 function musicrelease_enqueue_scripts(){
     
+    global $post;
     if ( $post->post_type == 'release' ) {
 
 
